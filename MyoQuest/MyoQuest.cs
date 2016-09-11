7d1de8ccb4543f8e45280d71fbfa12550314a5f5
Rory Roberts <rory.the.mover@gmail.com>
2016-09-11 09:36:35 +0100

    Esc to quit
    Stop the application from closing until user pushes esc

--- MyoQuest/MyoQuest.cs
+++ MyoQuest/MyoQuest.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyoQuest
 {
@@ -10,6 +6,9 @@ namespace MyoQuest
 	{
 		static void Main(string[] args)
 		{
+			while (Console.ReadKey().Key != ConsoleKey.Escape)
+			{
+			}
 		}
 	}
 }
