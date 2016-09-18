83bfc53bc26ba5e696ed76115752ad5bb9697f41
Rory Roberts <rory.the.mover@gmail.com>
2016-09-18 12:54:08 +0100

    Add MyoQuest.MyoController.UnityModule
    Added MyoController unity module, for handling all interface-object
    registration in the MyoQuest.MyoController project

---
+++ MyoQuest.MyoController.UnityModule/MyoControllerUnityModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using MyoQuest.Common;
+
+namespace MyoQuest.MyoController.UnityModule
+{
+	public sealed class MyoControllerUnityModule
+	{
+		private readonly IUnityContainer container;
+
+		public MyoControllerUnityModule(IUnityContainer container)
+		{
+			this.container = container;
+		}
+
+		public void Initialise()
+		{
+			this.container.RegisterType<IMyoDal, MyoDal>(new ContainerControlledLifetimeManager());
+
+			this.container.RegisterType<IMyoObjectFactory, MyoObjectFactory>();
+			this.container.RegisterType<IPoseToControllerStateConverter, StaticPoseToControllerStateConverter>();
+			this.container.RegisterType<IGameController, MyoControl>();
+		}
+	}
+}
