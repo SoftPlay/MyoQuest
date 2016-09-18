00b5268aad1bdd8a45edf3beee7a44f6021ce521
Rory Roberts <rory.the.mover@gmail.com>
2016-09-18 13:14:54 +0100

    Add MyoQuest.UnityModule
    Added Unity Module for the main project, allowing overall app setup.

---
+++ MyoQuest.UnityModule/UnityBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Unity;
+using MyoQuest.Game.UnityModule;
+using MyoQuest.MyoController.UnityModule;
+
+namespace MyoQuest.UnityModule
+{
+	public class UnityBootstrapper
+	{
+		private readonly IUnityContainer container;
+
+		public UnityBootstrapper()
+		{
+			this.container = new UnityContainer();
+		}
+
+		public void Initialise()
+		{
+			this.container.Resolve<GameUnityModule>().Initialise();
+			this.container.Resolve<MyoControllerUnityModule>().Initialise();
+		}
+	}
+}
